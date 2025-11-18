909217d9e856028465ad2b0132f195c8e8e3182d
saad36u <astabc93@gmail.com>
2025-11-18 16:35:40 +0200

    update env

--- server/server.js
+++ server/server.js
@@ -18,10 +18,7 @@ app.use(express.json());
 
 // اتصال MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => {
     console.error("MongoDB connection error:", err);
