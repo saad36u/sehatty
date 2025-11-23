29a3009caaabbb7cffdcc072b267c7d0f0442243
saad36u <139570812+saad36u@users.noreply.github.com>
2025-11-23 16:32:29 +0200

    Update SearchPage.jsx
    deleted galeb abo helal

--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -16,10 +16,9 @@ const sampleTransfers = [
   { fullName: "احمد ابراهيم سلامه الجبالي", personId: "804734739", birthYear: "1993", mobileNo: "0566007300", status: "معتمد", travelStatus: 0, returnReason: "", categoryId: "1", internalStatusCode: 1 },
   { fullName: "تامر اسعد محمد بدوي", personId: "801872748", birthYear: "1990", mobileNo: "0566422836", status: "معتمد", travelStatus: 0, returnReason: "", categoryId: "1", internalStatusCode: 1 },
   { fullName: "يوسف امين يوسف النزلي", personId: "801344375", birthYear: "1985", mobileNo: "0599832952", status: "معتمد", travelStatus: 0, returnReason: "", categoryId: "1", internalStatusCode: 1 },
-  { fullName: "نضال حسن جميل بياعه", personId: "456106434", birthYear: "1983", mobileNo: "0598451315", status: "تحت الاجراء", travelStatus: 0, returnReason: "", categoryId: "1", internalStatusCode: 1 },
+  { fullName: "نضال حسن جميل بياعه", personId: "456106434", birthYear: "1983", mobileNo: "0598451315", status: "معتمد", travelStatus: 0, returnReason: "", categoryId: "1", internalStatusCode: 1 },
   { fullName: "محمد سليم محمد صرصور", personId: "800584344", birthYear: "1985", mobileNo: "0592500304", status: "معتمد", travelStatus: 0, returnReason: "", categoryId: "1", internalStatusCode: 1 },
   { fullName: "إبراهيم سفيان رضوان الريفي", personId: "408479186", birthYear: "2003", mobileNo: "0597352661", status: "تحت الاجراء", travelStatus: 0, returnReason: "", categoryId: "1", internalStatusCode: 1 },
-  { fullName: "غالب محمدغالب سليم ابوهلال", personId: "402923080", birthYear: "1996", mobileNo: "0592601111", status: "تحت الاجراء", travelStatus: 0, returnReason: "", categoryId: "1", internalStatusCode: 1 },
   { fullName: "عمرو سليم خلف ابو سيدو", personId: "802154443", birthYear: "1988", mobileNo: "0597718871", status: "معتمد", travelStatus: 0, returnReason: "", categoryId: "1", internalStatusCode: 1 },
   { fullName: "ابراهيم محمد محمود ابو حطب", personId: "906726971", birthYear: "1981", mobileNo: "0599908959", status: "معتمد", travelStatus: 0, returnReason: "", categoryId: "1", internalStatusCode: 1 }
 ];
